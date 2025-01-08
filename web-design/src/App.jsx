59f33d96b5a91b0c98e2feadc4865aecff290c81
sai <saivarun.puri@gmail.com>
2025-01-08 14:49:51 +0530

    fourth

--- web-design/src/App.jsx
+++ web-design/src/App.jsx
@@ -4,13 +4,13 @@ import 'font-awesome/css/font-awesome.min.css';
 
 import PageOne from "./pages/pageone";
 import Pagetwo from "./pages/pagetwo";
-import PageThree from "./pages/pagethree";
 import AchievementsPage from "./pages/AchievementsPage";
 import Courses from "./pages/Courses";
 import Choose from "./pages/Choose";
 import Trusted from "./pages/Trusted";
 import Testimonials from "./pages/Testimonials";
 import Footer from "./pages/Footer";
+import Services from "./pages/Services";
 
 const App = () => {
   return (
@@ -18,7 +18,7 @@ const App = () => {
       {/* Render Page One */}
       <PageOne />
       <Pagetwo />
-      <PageThree/>
+      <Services/>
       <AchievementsPage/>
       <Courses/>
       <Choose/>
